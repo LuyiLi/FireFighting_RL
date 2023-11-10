003a3da262feaa1f6bdc9882f99feb52a96faa0b
Yongzhou Pan <yongzh.pan@gmail.com>
2023-11-10 23:40:17 +0800

    submit modified ACNet

--- ACNet_pytorch.py
+++ ACNet_pytorch.py
@@ -15,6 +15,11 @@ class ACNet(nn.Module):
         super(ACNet, self).__init__()
         self.inputs = torch.zeros(batch_size, 4, GRID_SIZE, GRID_SIZE)
         self.water_res = torch.zeros(batch_size, 1)
+        self.a_size = a_size
+        self.batch_size = batch_size
+        self.trainer = trainer
+        self.learning_rate = learning_rate
+        self.policy = None
 
         # Define ACNet layers
         # 4 maps for each agent
@@ -45,34 +50,16 @@ class ACNet(nn.Module):
         self.state_init = [c_init, h_init]
         self.state_in = (torch.zeros(1, RNN_SIZE, dtype=torch.float32), torch.zeros(1, RNN_SIZE, dtype=torch.float32)) # (cell_state, hidden_state)
 
-        self.policy, self.value, self.state_out, _ = self._build_net(self.inputs, self.water_res, a_size)
+        # self.policy, self.value, self.state_out, _ = self._build_net(self.inputs, self.water_res, a_size)
+        # self.policy, self.value, self.state_out, _ = self._build_net(self.inputs, self.water_res)
 
-        if TRAINING:
-            self.actions = torch.zeros(batch_size, dtype=torch.int64)
-            # print(self.actions.shape)
-            self.actions_onehot = F.one_hot(self.actions, a_size).type(torch.float32)
-            # print(self.actions_onehot.shape)
-            self.target_v = torch.zeros(batch_size, dtype=torch.float32)
-            self.advantages = torch.zeros(batch_size, dtype=torch.float32)
-            self.responsible_outputs = torch.sum(self.policy * self.actions_onehot, dim=1)
-
-            # Loss Functions
-            self.value_loss = 0.5 * torch.sum((self.target_v - self.value.view(-1))**2)
-            self.entropy = -0.01 * torch.sum(self.policy * torch.log(torch.clamp(self.policy, 1e-10, 1.0)))
-            self.policy_loss = -torch.sum(torch.log(torch.clamp(self.responsible_outputs, 1e-15, 1.0)) * self.advantages)
-            self.loss = self.value_loss + self.policy_loss - self.entropy
-
-            # Get gradients from local network using local losses and
-            # normalize the gradients using clipping
-            trainable_vars = list(self.parameters())
-            self.gradients = torch.autograd.grad(self.loss, trainable_vars, create_graph=True)
-            self.var_norms = torch.norm(torch.cat([v.view(-1) for v in trainable_vars]))
-            self.grad_norms = torch.nn.utils.clip_grad_norm_(self.gradients, GRAD_CLIP)
-            self.apply_grads = trainer(self.parameters(), lr=learning_rate)
+        # Initialize model weights
+        self.apply(weights_init)
 
         print("QAQ! The network is working!")
 
-    def _build_net(self, inputs, water_res, a_size):
+    # def _build_net(self, inputs, water_res, a_size):
+    def forward(self, inputs, water_res):
         conv1 = F.relu(self.conv1(inputs))
         conv1a = F.relu(self.conv1a(conv1))
         conv1b = F.relu(self.conv1b(conv1a))
@@ -93,30 +80,58 @@ class ACNet(nn.Module):
         h2 = F.relu(self.fc2(h1))
         self.h3 = F.relu(self.fc3(h2 + hidden_input))
 
-        # rnn_in = self.h3.unsqueeze(0)
+        rnn_in = self.h3.unsqueeze(0)
         # TODO: Modify time step HERE.
-        sequence_length = 1
-        rnn_in = self.h3.unsqueeze(0).unsqueeze(0).expand(sequence_length, -1, -1)
+        # sequence_length = 1
+        # rnn_in = self.h3.unsqueeze(0).unsqueeze(0).expand(sequence_length, -1, -1)
         lstm_out, lstm_state = self.lstm(rnn_in)
         lstm_c, lstm_h = lstm_state
-        state_out = (lstm_c[:1, :], lstm_h[:1, :])
+        self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
         rnn_out = lstm_out.view(-1, RNN_SIZE)
 
         policy_layer = self.fc_policy(rnn_out)
-        policy = F.softmax(policy_layer, dim=1)
+        self.policy = F.softmax(policy_layer, dim=1)
         policy_sig = torch.sigmoid(policy_layer)
-        value = self.fc_value(rnn_out)
+        self.value = self.fc_value(rnn_out)
 
-        return policy, value, state_out, policy_sig
+        return self.policy, self.value, self.state_out, policy_sig
     
-    # Function for weights initialization
-    def weights_init(m):
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-            init.xavier_uniform_(m.weight.data)
-            init.constant_(m.bias.data, 0)
-        elif isinstance(m, nn.LSTM):
-            for param in m.parameters():
-                if len(param.shape) >= 2:
-                    init.orthogonal_(param.data)
-                else:
-                    init.constant_(param.data, 0)
+    def optimize_model(self):
+        if self.policy != None:   
+            self.actions = torch.zeros(self.batch_size, dtype=torch.int64)
+            # print(self.actions.shape)
+            self.actions_onehot = F.one_hot(self.actions, self.a_size).type(torch.float32)
+            # print(self.actions_onehot.shape)
+            self.target_v = torch.zeros(self.batch_size, dtype=torch.float32)
+            self.advantages = torch.zeros(self.batch_size, dtype=torch.float32)
+            self.responsible_outputs = torch.sum(self.policy * self.actions_onehot, dim=1)
+
+            # Loss Functions
+            self.value_loss = 0.5 * torch.sum((self.target_v - self.value.view(-1))**2)
+            self.entropy = -0.01 * torch.sum(self.policy * torch.log(torch.clamp(self.policy, 1e-10, 1.0)))
+            self.policy_loss = -torch.sum(torch.log(torch.clamp(self.responsible_outputs, 1e-15, 1.0)) * self.advantages)
+            self.total_loss = self.value_loss + self.policy_loss - self.entropy
+
+            # Get gradients from local network using local losses and
+            # normalize the gradients using clipping
+            trainable_vars = list(self.parameters())
+            self.gradients = torch.autograd.grad(self.total_loss, trainable_vars, create_graph=True)
+            self.var_norms = torch.norm(torch.cat([v.view(-1) for v in trainable_vars]))
+            self.grad_norms = torch.nn.utils.clip_grad_norm_(self.gradients, GRAD_CLIP)
+            self.apply_grads = self.trainer(self.parameters(), lr=self.learning_rate)
+
+        else:
+            raise ValueError('The policy has not been calculated and equal to \'None\'.')
+        
+    
+# Function for weights initialization
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        init.xavier_uniform_(m.weight.data)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.constant_(param.data, 0)
